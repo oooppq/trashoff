1513cc74d221a3804a197edaa9b8a5f1680b1f7e
DaegyunOh <oooppq@naver.com>
2022-08-28 07:49:44 +0900

    all front part merged

--- rank_app/urls.py
+++ rank_app/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path,include
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', ranking, name='rank'),
+    path('main/', include('main_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
