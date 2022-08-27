bffeb97d395c33ec9732f9a27ccff5d7b822c841
0921sean <98502180+0921sean@users.noreply.github.com>
2022-08-27 22:19:05 +0900

    Univ, Throw, Quest 모델 생성

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+class Place(models.Model):
+    place_name = models.CharField(max_length=20)
+
+class Trashcan(models.Model):
+    place_id = models.ForeignKey(Place, on_delete=models.CASCADE)
+    
+
+class University(models.Model):
+    univ_name = models.CharField(max_length=20, null=True)
+    student_num = models.IntegerField(default=0, null=True)
+    throw_num = models.IntegerField(default=0, null=True)
+
+class Throwing(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    trashcan_id = models.ForeignKey(Trashcan, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+
+class Quest(models.Model):
+    place_id = models.ForeignKey(Place, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
