e5531e034dac9fcd2d0a16bd272798fcabee8fe6
DaegyunOh <oooppq@naver.com>
2022-08-28 01:33:50 +0900

    all model merged

--- quest_app/models.py
+++ quest_app/models.py
@@ -5,3 +5,4 @@ from rank_app.models import Place
 class Quest(models.Model):
     place_id = models.ForeignKey(Place, on_delete=models.CASCADE) # 퀘스트가 이뤄지는 장소
     content = models.CharField(max_length=200) # 퀘스트 설명
+    
