9d6165c33d9c2061f33d54933765338c117f95d5
danhan <danhandev@gmail.com>
2022-08-28 02:13:01 +0900

    Quest 모델 수정

--- quest_app/models.py
+++ quest_app/models.py
@@ -1,7 +1,22 @@
+from code import interact
+import string
 from django.db import models
 from rank_app.models import Place
-
+from accounts.models import User
 # Create your models here.
+
+
 class Quest(models.Model):
-    place_id = models.ForeignKey(Place, on_delete=models.CASCADE) # 퀘스트가 이뤄지는 장소
-    content = models.CharField(max_length=200) # 퀘스트 설명
+    place_id = models.ForeignKey(
+        Place, on_delete=models.CASCADE)  # 퀘스트가 이뤄지는 장소
+    title = models.CharField(max_length=100)  # 퀘스트 제목
+    content = models.CharField(max_length=200)  # 퀘스트 설명
+    level = models.IntegerField()  # 참여가능한 최소 레벨
+    reward = models.CharField(max_length=100)  # 퀘스트 보상
+
+# 유저가 성공한 퀘스트 목록
+
+
+class QuestUser(models.Model):
+    quest_id = models.ForeignKey(Quest, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
