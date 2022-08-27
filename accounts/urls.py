56c23fdf33f3296d241296df948260c2c3f5bdff
DaegyunOh <oooppq@naver.com>
2022-08-28 02:18:02 +0900

    my page added

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
+    path('mypage/', views.myPage, name='mypage'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
